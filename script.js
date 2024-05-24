af60dea3d758ac85a1891632a814de687e85f780
TosinoAce <tosinjoereal301@gmail.com>
2024-05-24 12:48:38 +0100

    Second commit

--- script.js
+++ script.js
@@ -48,12 +48,15 @@ themeToggle.addEventListener("click", () => {
   }
 });
 
-const hamburgerIndicator = document.querySelector(".barContainer > img")
 hamburger.addEventListener("click", () => {
   mobileNav.classList.toggle("mobile-open");
   if(mobileNav.classList.contains("mobile-open")){
-    hamburgerIndicator.setAttribute("src", "images/x-lg.svg");
+    hamburger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+  </svg>`;
   } else {
-    hamburgerIndicator.setAttribute("src", "images/list.svg");
+    hamburger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+  </svg>`;
   }
 })
